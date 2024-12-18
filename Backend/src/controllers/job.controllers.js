d993625732356df148b3ec7c416a10e98394df01
Muskan Singh <spariharmuskan1106@gmail.com>
2024-12-18 22:14:36 +0530

    Added frontend setup

---
+++ Backend/src/controllers/job.controllers.js
@@ -0,0 +1,6 @@
+import { Job } from "../models/job.model.js";
+import { asynchandler } from "../utils/asynchandler.js";
+import { ApiError } from "../utils/Apierrors.js";
+import { ApiResponse } from "../utils/Apiresponses.js";
+
+const postJob = asynchandler(async (req, res) => {});
